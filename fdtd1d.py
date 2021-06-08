20cd446ebd13282703ef40ffd8854859306acaf0
Enzo Isnard <enzoisnard@gmail.com>
2021-06-08 13:37:30 +0200

    added a source term

--- fdtd1d.py
+++ fdtd1d.py
@@ -7,16 +7,23 @@ import matplotlib.pyplot as plt
 H has only one component along y and E only along z
 
 H_y and E_z only depend on x.
+
+We use a normalized version of E: Ẽ_z = sqrt(eps/mu)*E
+
+dt/(eps*dx)*sqrt(eps/mu) = dt/(sqrt(eps*mu)*dx) = dt*c/dx = 1/2
+
+(dt/(mu*dx))*sqrt(mu/eps) = dt*c/dx = 1/2
 """
 
+# eps_0 = 8.85418782E-12 
+# mu_0 = 4*np.pi*1E-7
 L = 1
-Tmax = 100
+Tmax = 10
 eps = 10
 mu = 10
 c = (eps*mu)**(-1/2)
 dx=0.01
-courant = 0.5
-dt = (dx*courant)/c 
+dt = dx/(2*c)
 
 t = np.arange(0,Tmax,dt)
 x = np.arange(0,L,dx)
@@ -29,16 +36,20 @@ print(f"nx={nx}")
 
 H = np.zeros((nt,nx))
 E = np.zeros((nt,nx))
-H[0] = np.exp(-(x-L/2)**2/0.01)
-E[0] = np.exp(-(x-L/3)**2/0.01)
-
+kSource = int(nx/2)
+source = 1*np.exp(-(t-Tmax/2)**2)
+# E[0,iSource] = 1
+#E[0] = np.exp(-100*(x-L/2)**2)
+# E[0] = np.sin(2*np.pi*x)
+E[0,kSource]=source[0]
 for i in range(nt-1):
 
-    H[i+1,:-1] = H[i,:-1]+dt/(dx*mu)*(E[i,1:]-E[i,:-1])
-    H[i+1,-1]=H[i,-1]+dt/(dx*mu)*(-E[i,-1])
+    H[i+1,:-1] = H[i,:-1]+1/2*(E[i,1:]-E[i,:-1])
+    H[i+1,-1] = H[i+1,-2]
 
-    E[i+1,0]=E[i,0]+dt*(dx*mu)*(H[i+1,0])
-    E[i+1,1:] = E[i,1:]+dt*(dx*eps)*(H[i+1,1:]-H[i+1,:-1])
+    E[i+1,1:] = E[i,1:]+1/2*(H[i+1,1:]-H[i+1,:-1])
+    E[i+1,0]=E[i+1,1]
+    E[i+1,kSource] = source[i+1]
 
 for i in range(nt):
     y = H[i]
@@ -46,12 +57,16 @@ for i in range(nt):
     if i==0:
         line, = plt.plot(x,y)
         line2, = plt.plot(x,y2)
-        plt.ylim([-20,20])
+        plt.ylim([-3,3])
         plt.legend(["$H_y$","$E_z$"])
+        plt.title(f"Step n°1 ($\\eps={eps}$ and $\\mu={mu}$)")
     else:
         line.set_ydata(y)
         line2.set_ydata(y2)
-    plt.pause(0.01)
+        plt.title(f"Etape n°{i+1} ($\\eps={eps}$ and $\\mu={mu}$)")
+    plt.pause(0.1)
 
 plt.show()
 
+# plt.plot(t,H[:,0])
+# plt.show()
